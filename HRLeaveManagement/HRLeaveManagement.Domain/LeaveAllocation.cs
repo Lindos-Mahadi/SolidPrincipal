ed888efefbc9a0f5ee7e956ab0c347dacbffba80
lindos mahadi <lindosmahadi@gmail.com>
2023-10-24 20:41:44 +0600

    #91 Create The Domain Project

---
+++ HRLeaveManagement/HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set;}
+    }
+}
