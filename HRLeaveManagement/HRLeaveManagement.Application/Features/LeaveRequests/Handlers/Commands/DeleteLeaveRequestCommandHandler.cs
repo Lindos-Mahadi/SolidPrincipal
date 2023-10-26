223235a05b8491f1abe410ee22280ac2bbe33093
lindos mahadi <lindosmahadi@gmail.com>
2023-10-26 13:55:41 +0600

    #97 Delete Implementation and MediatR Part-Four

---
+++ HRLeaveManagement/HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HRLeaveManagement.Application.Persistance.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.Get(request.Id);
+            //await _leaveRequestRepository.Delete(Convert.ToInt32(leaveRequest));
+            await _leaveRequestRepository.Delete(leaveRequest.Id);
+            return Unit.Value;
+        }
+    }
+}
