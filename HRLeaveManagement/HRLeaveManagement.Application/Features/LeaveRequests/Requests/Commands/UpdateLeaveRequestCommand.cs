822e30e9cba1b41b04cfb146b71b737994ac94d9
lindos mahadi <lindosmahadi@gmail.com>
2023-10-26 13:08:58 +0600

    #97 All Update Dtos Implementing MediatR Part-Four

---
+++ HRLeaveManagement/HRLeaveManagement.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
@@ -0,0 +1,18 @@
+﻿using HRLeaveManagement.Application.DTOs.LeaveAllocation;
+using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequests.Requests.Commands
+{
+    public class UpdateLeaveRequestCommand : IRequest<Unit>
+    {
+        //public int Id { get; set; }
+        public LeaveRequestDto LeaveRequestDto { get; set; }
+        //public ChangeLeaveRequestApproveDto ChangeLeaveRequestApproveDto { get; set; }
+    }
+}
