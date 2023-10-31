3b39d88b52679c7f2d9d57348a7dd98ac53e7eb3
lindos mahadi <lindosmahadi@gmail.com>
2023-10-31 13:06:30 +0600

    #98 Implement All Dto classes for adding validation

--- HRLeaveManagement/HRLeaveManagement.Application/DTOs/LeaveType/CreateLeaveTypeDto.cs
+++ HRLeaveManagement/HRLeaveManagement.Application/DTOs/LeaveType/CreateLeaveTypeDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HRLeaveManagement.Application.DTOs.LeaveType
 {
-    public class CreateLeaveTypeDto
+    public class CreateLeaveTypeDto : ILeaveTypeDto
     {
         public string Name { get; set; }
         public int DefaultDays { get; set; }
