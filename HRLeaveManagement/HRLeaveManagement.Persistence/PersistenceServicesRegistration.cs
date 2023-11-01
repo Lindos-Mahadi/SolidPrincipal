dd581ba97ea8e65895a135e698cc4a0aec8efedd
lindos mahadi <lindosmahadi@gmail.com>
2023-11-01 09:52:07 +0600

    #102 PersistenceServicesRegistration Implement Persistence Layer

---
+++ HRLeaveManagement/HRLeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,33 @@
+﻿using HRLeaveManagement.Application.Persistance.Contracts;
+using HRLeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
