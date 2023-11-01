1c56094ba6ffd7a9d2676e2cf1f2414adfd77dd6
lindos mahadi <lindosmahadi@gmail.com>
2023-11-01 09:42:55 +0600

    #102 All other type Generic Implementation

---
+++ HRLeaveManagement/HRLeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,39 @@
+﻿using HRLeaveManagement.Application.Persistance.Contracts;
+using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+
+        public LeaveAllocationRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+        {
+            var leaveRequest = await _dbContext.LeaveAllocation
+               .Include(q => q.LeaveType)
+               .FirstOrDefaultAsync(q => q.Id == id);
+
+            return leaveRequest;
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationWithDetails()
+        {
+            var leaveRequest = await _dbContext.LeaveAllocation
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+
+            return leaveRequest;
+        }
+    }
+}
---
+++ HRLeaveManagement/HRLeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,46 @@
+﻿using HRLeaveManagement.Application.Persistance.Contracts;
+using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+        public LeaveRequestRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? ApprovalStatus)
+        {
+            leaveRequest.Approved = ApprovalStatus;
+            _dbContext.Entry(leaveRequest).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<LeaveRequest>> GetAllLeaveRequestWithDetails()
+        {
+            var leaveRequest = await _dbContext.LeaveRequest
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+
+                return leaveRequest;
+                
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _dbContext.LeaveRequest
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
+            return leaveRequest;
+        }
+    }
+}
