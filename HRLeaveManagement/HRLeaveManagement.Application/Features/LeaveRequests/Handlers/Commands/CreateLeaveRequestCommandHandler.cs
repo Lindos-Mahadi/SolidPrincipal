1e568c96a883a8826e8adcbe814e0e56e2c19eb3
lindos mahadi <lindosmahadi@gmail.com>
2023-11-01 10:49:06 +0600

    #103 IEmailSender Implement in CreateLeaveRequestCommandHandler

--- HRLeaveManagement/HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ HRLeaveManagement/HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -11,6 +11,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HRLeaveManagement.Application.Contracts.eMailFrastucture;
+using HRLeaveManagement.Application.Models;
+using System.Net.Mail;
 
 namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
 {
@@ -18,12 +21,14 @@ namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IEmailSender _emailSender;
         private readonly IMapper _mapper;
 
-        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository, IEmailSender emailSender, IMapper mapper )
         {
             _leaveRequestRepository = leaveRequestRepository;
             _leaveTypeRepository = leaveTypeRepository;
+            _emailSender = emailSender;
             _mapper = mapper;
         }
         public async Task<BasedCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
@@ -46,6 +51,25 @@ namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
             response.Success = true;
             response.Message = "Creation Successful";
             response.Id = leaveRequest.Id;
+
+            // Email Sender
+            var email = new Email
+            {
+                To = "puremahadi.it@gmail.com",
+                Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} " +
+                        $"has been submitted successfully.",
+                Subject = "Leave Request Submitted"
+            };
+            try
+            {
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception)
+            {
+
+                // Log or handle error, but dont thorw.....;
+            }
+
             return response;
         }
     }
