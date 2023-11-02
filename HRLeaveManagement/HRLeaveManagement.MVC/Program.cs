509c7800f918bea5d4545cdc19da3f5b58ee7b21
lindos mahadi <lindosmahadi@gmail.com>
2023-11-02 20:25:12 +0600

    #116 SetUp Program.cs & Custom Api Clients and Base Code

--- HRLeaveManagement/HRLeaveManagement.MVC/Program.cs
+++ HRLeaveManagement/HRLeaveManagement.MVC/Program.cs
@@ -1,7 +1,9 @@
 using HR.LeaveManagement.MVC.Services.Base;
+using HRLeaveManagement.MVC.Contracts;
 using HRLeaveManagement.MVC.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace HRLeaveManagement.MVC
 {
@@ -17,11 +19,15 @@ namespace HRLeaveManagement.MVC
             //    options.UseSqlServer(connectionString));
 
             builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:44383"));
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddSingleton<ILocalStorageService, ILocalStorageService>();
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
---
+++ HRLeaveManagement/HRLeaveManagement.MVC/Services/Base/Client.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace HRLeaveManagement.MVC.Services.Base
+{
+    public partial class Client : IClient
+    {
+        private HttpClient _httpClient;
+        public HttpClient HttpClient 
+        {
+            get
+            {
+                return  _httpClient;
+            }
+        }
+    }
+}
