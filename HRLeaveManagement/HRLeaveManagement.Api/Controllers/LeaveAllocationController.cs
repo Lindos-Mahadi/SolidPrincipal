666615d357e5be67bcb40f12ddf7d8f36bda8002
lindos mahadi <lindosmahadi@gmail.com>
2023-11-02 11:59:45 +0600

    #108 Created Leave Allocation And RequestController

---
+++ HRLeaveManagement/HRLeaveManagement.Api/Controllers/LeaveAllocationController.cs
@@ -0,0 +1,64 @@
+﻿using HRLeaveManagement.Application.DTOs.LeaveAllocation;
+using HRLeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+using HRLeaveManagement.Application.Features.LeaveAllocations.Requests.Quries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HRLeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveAllocationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveAllocationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: api/<LeaveAllocationController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveAllocationDto>>> Get()
+        {
+            var leaveAllocaton = await _mediator.Send(new GetLeaveAllocationListRequest());
+            return Ok(leaveAllocaton);
+        }
+
+        // GET api/<LeaveAllocationController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveAllocationDto>> Get(int id)
+        {
+            var leaveAllocaton = await _mediator.Send(new GetLeaveAllocationDetailRequest { Id = id });
+            return Ok(leaveAllocaton);
+        }
+
+        // POST api/<LeaveAllocationController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveAllocationDto leaveAllocaton)
+        {
+            var command = new CreateLeaveAllocationCommand { LeaveAllocationDto = leaveAllocaton };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        // PUT api/<LeaveAllocationController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put( [FromBody] UpdateLeaveAllocationDto leaveAllocaton)
+        {
+            var command = new UpdateLeaveAllocationCommand { LeaveAllocationDto = leaveAllocaton };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // DELETE api/<LeaveAllocationController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new DeleteLeaveAllocationCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ HRLeaveManagement/HRLeaveManagement.Api/Controllers/LeaveRequestController.cs
@@ -0,0 +1,73 @@
+﻿using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using HRLeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HRLeaveManagement.Application.Features.LeaveRequests.Requests.Quries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HRLeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveRequestController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: api/<LeaveRequestController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveRequestDto>>> Get()
+        {
+            var leaveRequest = await _mediator.Send(new GetLeaveRequestListRequest());
+            return Ok(leaveRequest);
+        }
+
+        // GET api/<LeaveRequestController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequestDto>> Get(int id)
+        {
+            var leaveRequest = await _mediator.Send(new GetLeaveRequestDetailRequest { Id = id });
+            return Ok(leaveRequest);
+        }
+
+        // POST api/<LeaveRequestController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveRequestDto leaveRequest)
+        {
+            var command = new CreateLeaveRequestCommand { LeaveRequestDto = leaveRequest };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        // PUT api/<LeaveRequestController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] UpdateLeaveRequestDto leaveRequest)
+        {
+            var command = new UpdateLeaveRequestCommand { Id = id, LeaveRequestDto = leaveRequest };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // PUT api/<LeaveRequestController>/ ChangeApproval
+        [HttpPut("ChangeApproval/{id}")]
+        public async Task<ActionResult> ChangeApproval(int id, [FromBody] ChangeLeaveRequestApproveDto changeLeaveRequestApproval)
+        {
+            var command = new UpdateLeaveRequestCommand { Id = id, ChangeLeaveRequestApproveDto = changeLeaveRequestApproval };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // DELETE api/<LeaveRequestController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new DeleteLeaveRequestCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
